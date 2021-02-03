02f420ca37b8676379e50e06e732acaab311605c
Jordan Slim <sjordanslim@gmail.com>
2021-02-03 15:24:17 -0600

    Finished ToDo list

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,25 @@
 // import logo from './logo.svg';
 import {useState} from 'react';
 
-import logo from './tek_logo.png';
+//import logo from './tek_logo.png';
 import './App.css';
 import Todo from './Components/Todo';
 
 function App() {
 
   const [todos, setTodos] = useState(["eat","sleep","code","pray","repeat"]);  //sample todo items.  these will need to be changed in your app.  Just filler todos, although they are quite important!
+  const [items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+      setTodos(even.target.value);
+  };
+
+const listItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, todos];
+    });
+    setTodos("");
+};
 
   return (
     <div className="App">
@@ -16,10 +28,15 @@ function App() {
         <div>
          <br/>
           <p>Create a Todo List App</p>
-          <Todo item="1"/>
+          <input type = "text" value = {todo } placeholder="Add Items" onChange = {itemEvent}/>;
+          <button onClick={(listItems)}> + </button>
+        <Todo item="1"/>
           <Todo item="2"/>
           <Todo item="3"/>
-          {todos.map( (t,i) => <Todo key={i} item={t}/>)}
+          <ol>
+          {items.map((itemval) =>{ 
+          return <li>{itemval} </li>;})}
+          </ol>
         </div>
       </header>
     </div>
